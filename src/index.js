73bf1e7bd3c02be4c50f1e1c2e0456900677316e
terunrun <terunrun@gmail.com>
2020-02-01 17:57:16 +0900

    Complete why-immutability-is-important

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    // すべてのコンストラクタはsuper()の呼び出しから始める
-    super(props);
-    // stateの初期化
-    // stateはmutableなもの
-    // チュートリアルコードではpropsと同要素名となっているが、違うものを指すため変更している
-    this.state = {displayValue: null};
-  }
+  // 各マスの状態は盤全体で管理するため、Stateを保持する必要がなくなる
+  // constructor(props) {
+  //   // すべてのコンストラクタはsuper()の呼び出しから始める
+  //   super(props);
+  //   // stateの初期化
+  //   // stateはmutableなもの
+  //   // チュートリアルコードではpropsと同要素名となっているが、違うものを指すため変更している
+  //   this.state = {displayValue: null};
+  // }
 
   render() {
     return (
@@ -21,22 +22,55 @@ class Square extends React.Component {
       // setStateメソッドでstateのvalueを書き変える
       <button
         className="square"
-        onClick={() => {this.setState({displayValue: 'X'})}}
+        // クリック時にstateの所定の値を書き変える
+        // onClick={() => {this.setState({displayValue: 'X'})}}
+        // クリック時に親コンポーネントから渡された関数を実行する
+        onClick={() => this.props.onClick()}
       >
         {/* 受け取った引数はthis.propsで取得する */}
         {/* {this.props.value} */}
         {/* stateのvalueを取得する */}
-        {this.state.displayValue}
+        {/* {this.state.displayValue} */}
+        {/* 各マスは受け取った引数を表示するのみとする */}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // 盤全体を表すstateを用意する
+      // チュートリアルコードはnullだが、見た目のわかりやすさのため-を設定している
+      squares: Array(9).fill('-'),
+    };
+  }
+
+  handleClick(i) {
+    // 元々のstateを直接書き変えないようにするため、元のstateと同等のものを一時的に作成する
+    // チュートリアルコードではsquaresだが、実体が別であることを明示するため要素名を変更している
+    const temp_squares = this.state.squares.slice();
+    // クリックされたマス目だけ値を変更する
+    temp_squares[i] = 'X';
+    // stateを更新するにはあくまでもsetStateを用いる
+    this.setState({squares: temp_squares});
+  }
+
   renderSquare(i) {
     // 受け取った引数をvalueという名前のprposでSquareコンポーネントに渡す
     // propsはimmutableなもの
-    return <Square value={i}/>;
+    // return <Square value={i}/>;
+    // BoardのSquaresの値（各マスのその時の状態）を渡す
+    // return <Square value={this.state.squares[i]}/>;
+    // BoardのSquaresの値（各マスのその時の状態）と、クリック時の動作関数をonClickとして渡す
+      return(
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
